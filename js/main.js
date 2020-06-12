8b7e9932e61b8cb0532eac6fbfdb73f6c92b545b
Николай Карнаев <karnaev.nick@gmail.com>
2020-06-12 16:01:15 +0300

    Исправление замечаний

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ var CHECKINS = ['12:00', '13:00', '14:00'];
 var CHECKOUTS = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var APARTMENT_QUANTITY = 8;
 var MIN_PRICE = 1000;
 var MAX_PRICE = 25000;
 var MIN_ROOMS = 1;
@@ -14,9 +13,25 @@ var MIN_GUESTS = 1;
 var MAX_GUESTS = 10;
 
 var PIN_MIN_X = 0;
-var PIN_MAX_X = 1150;
 var PIN_MIN_Y = 130;
 var PIN_MAX_Y = 630;
+
+var apartmentQuantity = 8;
+
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var mapPins = document.querySelector('.map__pins');
+var map = document.querySelector('.map');
+
+var fragment = document.createDocumentFragment();
+
+var getElementWidth = function (element) {
+  var elementWidth = window.getComputedStyle(element, null).getPropertyValue('width');
+  return parseInt(elementWidth, 10);
+};
+
+var pinMaxX = getElementWidth(mapPins);
+var pinWidth = 0;
+
 
 var getRandomArrayElement = function (array) {
   var rand = Math.floor(Math.random() * array.length);
@@ -36,9 +51,9 @@ var createRandomArray = function (array) {
   return newArray;
 };
 
-var createApartmentsArray = function () {
+var createApartments = function (elementsQuantity) {
   var apartmentsArray = [];
-  for (var i = 0; i < APARTMENT_QUANTITY; i++) {
+  for (var i = 0; i < elementsQuantity; i++) {
     var apartment = {
       author: {
         avatar: 'img/avatars/user0' + (i + 1) + '.png'
@@ -59,7 +74,7 @@ var createApartmentsArray = function () {
       },
 
       location: {
-        x: getRandomNumber(PIN_MIN_X, PIN_MAX_X),
+        x: getRandomNumber(PIN_MIN_X + pinWidth, pinMaxX - pinWidth),
         y: getRandomNumber(PIN_MIN_Y, PIN_MAX_Y),
       }
     };
@@ -68,36 +83,49 @@ var createApartmentsArray = function () {
   return apartmentsArray;
 };
 
-var pinTemplate = document.querySelector('#pin').content;
-var mapPins = document.querySelector('.map__pins');
+var createPin = function (object) {
+  var pin = pinTemplate.cloneNode(true);
+  pin.style.left = object.location.x + 'px';
+  pin.style.top = object.location.y + 'px';
 
-var createPin = function (apartment) {
-  var newPin = pinTemplate.cloneNode(true);
-  newPin.querySelector('.map__pin').style.left = apartment.location.x + 'px';
-  newPin.querySelector('.map__pin').style.top = apartment.location.y + 'px';
+  var pinImg = pin.querySelector('img');
+  pinImg.src = object.author.avatar;
+  pinImg.alt = object.offer.title;
 
-  var pinImg = newPin.querySelector('img');
-  pinImg.src = apartment.author.avatar;
-  pinImg.alt = apartment.offer.title;
-
-  return newPin;
+  return pin;
 };
 
-var fragment = document.createDocumentFragment();
+var getLastElementOfClass = function (parent, childClass) {
+  return parent.querySelector(childClass + ':last-child');
+};
 
-var renderPins = function () {
-  var apartments = createApartmentsArray();
+// Определяем ширину генерируемых пинов: создаем один пин, добавляем в разметку, запоминаем его ширину, удаляем пин.
+// Последующие пины будем генерировать уже с учетом полученной ширины.
+var getPinWidth = function () {
+  var apartment = createApartments(1);
+  var pin = createPin(apartment[0]);
+  fragment.appendChild(pin);
 
-  for (var i = 0; i < apartments.length; i++) {
-    var pin = createPin(apartments[i]);
+  mapPins.appendChild(fragment);
+  var mapPin = getLastElementOfClass(mapPins, '.map__pin');
+  var width = getElementWidth(mapPin);
+
+  mapPin.parentNode.removeChild(mapPin);
+
+  return width;
+};
+
+var renderPins = function (array) {
+  for (var i = 0; i < array.length; i++) {
+    var pin = createPin(array[i]);
     fragment.appendChild(pin);
   }
-
   mapPins.appendChild(fragment);
 };
 
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
+pinWidth = getPinWidth();
+var apartments = createApartments(apartmentQuantity);
+renderPins(apartments);
 
-renderPins();
+map.classList.remove('map--faded');
 
